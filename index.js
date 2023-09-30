cf2dab6f5e16b85982e762ad515ee0bfd211e9bd
Marcia Ong <MrMarciaOng@gmail.com>
2023-10-01 02:32:23 +0800

    feat: check lido rebase event endpoint and general bot improvements

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const TelegramBot = require("node-telegram-bot-api");
 const { v4: uuidv4 } = require("uuid");
 const { Pool } = require("pg");
 const cron = require("node-cron");
+const dayjs = require("dayjs");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
-
+let isScheduleActive = false;
 if (!token) {
   console.error("Error: TELEGRAM_BOT_TOKEN is not set in the .env file.");
   process.exit(1);
@@ -104,52 +105,100 @@ function isValidEthAddress(address) {
   return /^(0x)[0-9A-Fa-f]{40}$/.test(address);
 }
 
-// Schedule a task to run every day at 12:30 UTC gave 30mins buffer time for rebase to happen
-cron.schedule(
-  "30 12 * * *",
-  async () => {
-    console.log("Running a task every day at 12:15 UTC");
-
-    try {
-      // Start a connection from the pool
-      const client = await pool.connect();
-
-      try {
-        // Fetch all users from the database
-        const result = await client.query(
-          "SELECT * FROM telegram_lido_tracking"
-        );
-        const users = result.rows;
-        console.log("users to notify", users.length);
-        // For each user, fetch the latest block rewards and send a Telegram message
-        for (const user of users) {
-          try {
-            const address = user.wallet_address;
-            const chatId = user.chat_id; // Use the stored chatId here
-
-            const url = `https://stake.lido.fi/api/rewards?address=${address}&currency=usd&onlyRewards=false&archiveRate=true&skip=0&limit=10`;
-            const response = await axios.get(url);
-            const data = response.data;
-
-            const short_message = formatSimpleLidoStatsMessage(data);
-            bot.sendMessage(chatId, short_message, { parse_mode: "Markdown" });
-          } catch (error) {
-            console.error(`Error notifying user ${user.user_id}: ${error}`);
-          }
-        }
-      } finally {
-        // Release the client back to the pool
-        client.release();
-      }
-    } catch (error) {
-      console.error("Error fetching users from the database:", error);
-    }
-  },
-  {
-    scheduled: true,
-    timezone: "UTC",
+let hasRebasedToday = false;
+let resetRebaseCronJob;
+let rebaseCronJob;
+bot.onText(/\/startBot/i, async (msg) => {
+  if (!checkAdmin(msg)) {
+    return;
   }
-);
+  startCronJobs();
+  const reply = `starting rewards schedule for all users`;
+  bot.sendMessage(chatId, reply, {
+    message_thread_id: msgThreadId,
+  });
+});
+
+function startCronJobs() {
+  if (isScheduleActive) {
+    console.log("Cron job is already running, resetting hasRebasedToday");
+    rebaseCronJob.stop();
+    resetRebaseCronJob.stop();
+  }
+  console.log("Starting cron jobs");
+  // Schedule a task to run every day at 12:00 UTC
+  rebaseCronJob = cron.schedule(
+    "* 12-13 * * *",
+    async () => {
+      console.log(
+        "Running a task every day at 12:00 UTC to rebase successfully"
+      );
+      if (hasRebasedToday) return;
+      const rebaseResults = await getStEthApr();
+      //check if rebase has happened today
+      if (rebaseResults.formattedDate.isSame(dayjs(), "day")) {
+        hasRebasedToday = true;
+        try {
+          // Start a connection from the pool
+          const client = await pool.connect();
+
+          try {
+            // Fetch all users from the database
+            const result = await client.query(
+              "SELECT * FROM telegram_lido_tracking"
+            );
+            const users = result.rows;
+            console.log("users to notify", users.length);
+            // For each user, fetch the latest block rewards and send a Telegram message
+            for (const user of users) {
+              try {
+                const address = user.wallet_address;
+                const chatId = user.chat_id; // Use the stored chatId here
+
+                const url = `https://stake.lido.fi/api/rewards?address=${address}&currency=usd&onlyRewards=false&archiveRate=true&skip=0&limit=10`;
+                const response = await axios.get(url);
+                const data = response.data;
+
+                const short_message = formatSimpleLidoStatsMessage(data);
+                bot.sendMessage(chatId, short_message, {
+                  parse_mode: "Markdown",
+                });
+              } catch (error) {
+                console.error(`Error notifying user ${user.user_id}: ${error}`);
+              }
+            }
+          } finally {
+            // Release the client back to the pool
+            client.release();
+          }
+        } catch (error) {
+          console.error("Error fetching users from the database:", error);
+        }
+      }
+    },
+    {
+      scheduled: true,
+      timezone: "UTC",
+    }
+  );
+
+  // schedule a task to check if rebase has happened then turn hasRebasedToday to false
+  resetRebaseCronJob = cron.schedule(
+    "* 14 * * *",
+    async () => {
+      if (hasRebasedToday === false) return;
+      console.log(
+        "Running a task every day at 13:00 UTC to reset hasRebasedToday"
+      );
+      hasRebasedToday = false;
+    },
+    {
+      scheduled: true,
+      timezone: "UTC",
+    }
+  );
+  isScheduleActive = true;
+}
 
 bot.onText(/\/deregister/, async (msg) => {
   const chatId = msg.chat.id;
@@ -187,7 +236,7 @@ function formatLidoStatsMessage(data) {
   // Extracting required information from data
   const { events, totals, averageApr, ethToStEthRatio, stETHCurrencyPrice } =
     data;
-  
+
   const firstEvent = events[0];
   const firstEventInfo = `
 ID: ${firstEvent.id}
@@ -235,8 +284,9 @@ function formatSimpleLidoStatsMessage(data) {
   // Extracting required information from data
   const { events, totals } = data;
 
-  const firstEvent = events[0];
+  const firstEvent = events.find((event) => event.type === "rewards");
   console.log(firstEvent);
+
   const latestRewardsInfo = `
 Latest Rewards: ${formatNumber(firstEvent.rewards, 8)} ETH
 Currency Change: $${parseFloat(firstEvent.currencyChange).toFixed(6)}
@@ -260,3 +310,56 @@ Total Currency Rewards: $${parseFloat(totals.currencyRewards).toFixed(6)}
 function formatNumber(num, dp = 6) {
   return parseFloat((Number(num) / 10 ** 18).toFixed(dp));
 }
+
+async function getStEthApr() {
+  const url = "https://eth-api.lido.fi/v1/protocol/steth/apr/last";
+
+  try {
+    const response = await axios.get(url);
+    const { timeUnix, apr } = response.data.data;
+
+    const formattedDate = dayjs.unix(timeUnix);
+
+    console.log("APR:", apr);
+    console.log("Time (yyyy-mm-dd):", formattedDate);
+    return { apr, formattedDate };
+  } catch (error) {
+    console.error("Error fetching APR:", error);
+  }
+}
+
+// Check if admin
+function checkAdmin(msg) {
+  // Usernames are case sensitive
+  const admins = ["Mr_Marcia_Ong"];
+  const chatId = msg.chat.id;
+  const msgThreadId = msg.message_thread_id;
+  const messageId = msg.message_id;
+  if (!admins.includes(msg.from.username)) {
+    bot.sendMessage(chatId, "You are not an admin to execute this command", {
+      message_thread_id: msgThreadId,
+      reply_to_message_id: messageId,
+    });
+    return false;
+  }
+  return true;
+}
+
+bot.onText(/\/stopBot/i, async (msg) => {
+  if (!checkAdmin(msg)) {
+    return;
+  }
+  const chatId = msg.chat.id;
+  const msgThreadId = msg.message_thread_id;
+  const reply = `Stopping rewards schedule.`;
+  bot.sendMessage(chatId, reply, {
+    message_thread_id: msgThreadId,
+  });
+  chatIdCronStatusMap[chatId] = false;
+  console.log("Cron job has been stopped");
+  resetRebaseCronJob.stop();
+  rebaseCronJob.stop();
+});
+
+
+startCronJobs();
